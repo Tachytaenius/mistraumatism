db52aa760dd7b5f495d5853b0ce590bac6031bb3
Tachytaenius <henryfthomson@gmail.com>
2026-02-09 11:23:35 +0000

    Commit debug level changes

--- levels/debug/init.lua
+++ levels/debug/init.lua
@@ -4,17 +4,37 @@ local info = {}
 
 -- self is game instance
 function info:createLevel()
+	-- Various debug situations commented out
+
 	self:initialiseMap(32, 24)
 	for x = 0, self.state.map.width - 1 do
 		for y = 0, self.state.map.height - 1 do
 			-- if x == 0 or x == self.state.map.width - 1 or y == 0 or y == self.state.map.height - 1 then
-			if self:distance(11, 11, x, y) >= 11 then
-				self:replaceTileInfo(x, y, {type = "wall", material = "marbleGreen"})
+			-- if self:distance(11, 11, x, y) >= 11 then
+			-- 	self:replaceTileInfo(x, y, {type = "wall", material = "marbleGreen"})
+			-- else
+			-- 	self:replaceTileInfo(x, y, {type = "sand", material = "quartz"})
+			-- end
+
+			if
+				x == 0 or x == self.state.map.width - 1 or y == 0 or y == self.state.map.height - 1 or
+				love.math.random() < 0.1
+			then
+				self:replaceTileInfo(x, y, {type = "wall", material = "granite"})
 			else
-				self:replaceTileInfo(x, y, {type = "sand", material = "quartz"})
+				self:replaceTileInfo(x, y, {type = "floor", material = "granite"})
 			end
 		end
 	end
+
+	for x = 0, 5 do
+		for y = 6, 10 do
+			self:replaceTileInfo(x, y, {type = "wall", material = "granite"})
+		end
+	end
+
+	local monster = self:placeMonster(6, 7, "hellKing")
+	monster.noAI = true
 
 	-- self:placeItem(1, 1, "tacticalArmour", "hyperPolymer")
 
@@ -33,13 +53,13 @@ function info:createLevel()
 	-- self:placeItem(7, 3, "largeMedkit", "plasticGreen")
 	-- self:placeItem(8, 3, "largeMedkit", "plasticGreen")
 
-	self:placeItem(24, 11, "healingRune", "slate")
+	-- self:placeItem(24, 11, "healingRune", "slate")
 	-- self:placeItem(11, 2, "healingRune", "slate")
 	-- self:placeItem(10, 3, "healingRune", "slate")
 	-- self:placeItem(11, 3, "healingRune", "slate")
 
-	self:placeItem(7, 7, "rocketLauncher", "polymer")
-	self:placeItem(7, 7, "rocket", "plasticBrown")
+	-- self:placeItem(7, 7, "rocketLauncher", "polymer")
+	-- self:placeItem(7, 7, "rocket", "plasticBrown")
 
 	-- self:placeItem(1, 1, "railgun", "polymer")
 	-- local cell = self:placeItem(1, 1, "railgunEnergyCell", "polymer")
@@ -83,12 +103,12 @@ function info:createLevel()
 	-- self:placeItem(1, 2, "knightlyArmour", "steel")
 	-- self:placeItem(2, 1, "longsword", "steel")
 
-	local opponent = self:placeMonster(16, 11, "hellNoble")
-	opponent.inventory[1].item = self:newItemData({
-		itemTypeName = "ornateKey", material = "steel",
-		lockName = "debugKey", breakOnUse = false
-	})
-	opponent.inventory.selectedSlot = 1
+	-- local opponent = self:placeMonster(16, 11, "hellNoble")
+	-- opponent.inventory[1].item = self:newItemData({
+	-- 	itemTypeName = "ornateKey", material = "steel",
+	-- 	lockName = "debugKey", breakOnUse = false
+	-- })
+	-- opponent.inventory.selectedSlot = 1
 
 	-- for _=1, 1 do
 	-- 	local monster = self:placeMonster(20, 11, "human", "armArtillery", "steel")
@@ -105,24 +125,62 @@ function info:createLevel()
 	-- end
 
 	-- self:placeKey(6, 2, "ornateKey", "iron", "debugKey")
-	self:replaceTileInfo(22, 11, {type = "sand", material = "quartz"})
-	for x = 23, 25 do
-		for y = 10, 12 do
-			self:replaceTileInfo(x, y, {type = "sand", material = "quartz"})
-		end
-	end
-	self:placeDoorItem(22, 11, "ornateDoor", "granite", false, "debugKey")
+	-- self:replaceTileInfo(22, 11, {type = "sand", material = "quartz"})
+	-- for x = 23, 25 do
+	-- 	for y = 10, 12 do
+	-- 		self:replaceTileInfo(x, y, {type = "sand", material = "quartz"})
+	-- 	end
+	-- end
+	-- self:placeDoorItem(22, 11, "ornateDoor", "granite", false, "debugKey")
 
 	return {
-		spawnX = 7,
-		spawnY = 11,
+		spawnX = 5,
+		spawnY = 5,
 		postLevelGen = function()
 			local player = self.state.player
 
-			player.currentWornItem = self:newItemData({itemTypeName = "knightlyArmour", material = "steel"})
+			-- player.currentWornItem = self:newItemData({itemTypeName = "knightlyArmour", material = "steel"})
 
-			player.inventory[1].item = self:newItemData({itemTypeName = "longsword", material = "steel"})
+			-- player.inventory[1].item = self:newItemData({itemTypeName = "longsword", material = "steel"})
+			-- player.inventory.selectedSlot = 1
+
+			-- local shotgun = self:newItemData({itemTypeName = "pumpShotgun", material = "steel"})
+			-- shotgun.chamberedRound = self:newItemData({
+			-- 	itemTypeName = "buckshotShell",
+			-- 	material = "plasticRed"
+			-- })
+			-- shotgun.cocked = true
+			-- local ammoCount = shotgun.itemType.magazineCapacity
+			-- for i = 1, ammoCount do
+			-- 	shotgun.magazineData[i] = self:newItemData({
+			-- 		itemTypeName = "buckshotShell",
+			-- 		material = "plasticRed"
+			-- 	})
+			-- end
+			-- player.inventory[1].item = shotgun
+			-- player.inventory.selectedSlot = 1
+
+			local shotgun = self:newItemData({itemTypeName = "autoShotgun", material = "polymer"})
+			player.inventory[1].item = shotgun
 			player.inventory.selectedSlot = 1
+			player.inventory[2].item = self:newItemData({itemTypeName = "pumpShotgun", material = "polymer"})
+			player.inventory[3].item = self:newItemData({itemTypeName = "sawnShotgun", material = "steel"})
+			for _, params in ipairs({
+				{
+					itemTypeName = "buckshotShell",
+					material = "plasticRed"
+				},
+				{
+					itemTypeName = "slugShell",
+					material = "plasticGreen"
+				}
+			}) do
+				for _= 1, 8 do
+					local item = self:newItemData(params)
+					local slot = self:getBestFreeInventorySlotForItem(player, item)
+					assert(slot and self:addItemToSlot(player, slot, item), "Item was not added to inventory")
+				end
+			end
 		end
 	}
 end
